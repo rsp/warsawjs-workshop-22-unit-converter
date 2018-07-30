0fabf4f4c59aaf261cdac93cab5c79fce10ba4a5
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-07-31 00:35:49 +0200

    Add complete temperature converter implementation

--- converters/temperature.js
+++ converters/temperature.js
@@ -10,14 +10,36 @@
 //
 // Return the result of conversion.
 
+const f2c = value => (value - 32) * 5 / 9;
+const c2f = value => value * 9 / 5 + 32;
+const k2c = value => value - 273.15;
+const c2k = value => value + 273.15;
+const k2f = value => c2f(k2c(value));
+const f2k = value => c2k(f2c(value));
+
 const temperature = (value, from, to) => {
-  if (from === 'K' && to === 'C') {
-    return value - 273.15;
+  if (from === to) {
+    return value;
+  }
+  if (from === 'C' && to === 'F') {
+    return c2f(value);
+  }
+  if (from === 'F' && to === 'C') {
+    return f2c(value);
   }
   if (from === 'C' && to === 'K') {
-    return value + 273.15;
+    return c2k(value);
   }
-  return 0;
+  if (from === 'K' && to === 'C') {
+    return k2c(value);
+  }
+  if (from === 'F' && to === 'K') {
+    return f2k(value);
+  }
+  if (from === 'K' && to === 'F') {
+    return k2f(value);
+  }
+  return undefined;
 };
 
 module.exports = {
