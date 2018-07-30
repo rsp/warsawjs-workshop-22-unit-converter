394d7344346c85edbb45396daa3b0457e9b81ed0
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-07-31 00:51:22 +0200

    Add code comments to temerature converter

--- converters/temperature.js
+++ converters/temperature.js
@@ -10,13 +10,31 @@
 //
 // Return the result of conversion.
 
+// Converting °C <-> °F using formulas:
+
+// T[°C] = (T[°F] - 32) × 5 / 9
 const f2c = value => (value - 32) * 5 / 9;
+
+// T[°F] = T[°C] × 9 / 5 + 32
 const c2f = value => value * 9 / 5 + 32;
+
+// Converting °C <-> K using formulas:
+
+// T[°C] = T[K] - 273.15
 const k2c = value => value - 273.15;
+
+// T[K] = T[°C] + 273.15
 const c2k = value => value + 273.15;
+
+// Converting °F <-> K indirectly using existing functions:
+
+// T[K] -> T[°C] -> T[°F]
 const k2f = value => c2f(k2c(value));
+
+// T[°F] -> T[°C] -> T[K]
 const f2k = value => c2k(f2c(value));
 
+// General function to covert between any units:
 const temperature = (value, from, to) => {
   if (from === to) {
     return value;
