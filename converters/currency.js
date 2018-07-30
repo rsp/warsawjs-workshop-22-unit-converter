b0766f61cf04a2c07d255fff27d98c1a68e8c55d
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-07-31 01:08:54 +0200

    Add caching to currency converter

--- converters/currency.js
+++ converters/currency.js
@@ -7,19 +7,33 @@ const date = '2018-01-02';
 const format = 'format=json';
 const json = true;
 
+// Cache to avoid making multiple requests for the same currency,
+// initially empty:
+const cache = {};
+
+// Helper function to make requests to the API version with caching:
 const rate = async (unit) => {
   try {
+    if (cache[unit]) {
+      return cache[unit];
+    }
     const url = `${base}/${unit.toLowerCase()}/${date}/?${format}`;
     const response = await request({ url, json });
-    return response.rates[0].mid;
+    const value = response.rates[0].mid;
+    cache[unit] = value;
+    return value;
   } catch (err) {
     return null;
   }
 };
 
+// Two helper functions so that we can write:
+// usd() instead of rate('usd') and:
+// eur() instead of rate('eur')
 const usd = () => rate('usd');
 const eur = () => rate('eur');
 
+// General function to convert between any currencies:
 const currency = async (value, from, to) => {
   if (from === to) {
     return value;
@@ -47,7 +61,4 @@ const currency = async (value, from, to) => {
 
 module.exports = {
   currency,
-  rate,
-  usd,
-  eur,
 };
