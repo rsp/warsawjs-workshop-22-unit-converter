6bb9e7e6bb6fe816432fbaa890afbf05a70286d9
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-07-31 02:57:33 +0200

    Update tape tests

--- tests/tape/converters.temperature.test.js
+++ tests/tape/converters.temperature.test.js
@@ -8,9 +8,28 @@ const example = x => x + 1;
 
 test('example', (t) => {
   t.plan(1);
-  t.equal(example(10), 11);
+  t.equal(example(10), 11, 'should be ok');
 });
 
-test('temperature converter', (t) => {
+test('temperature', (t) => {
+
+  t.test('converter', (t) => {
+
+    t.ok(temperature, 'should be defined');
+
+    t.equal(typeof temperature, 'function', 'should be a function');
+
+    const x = temperature(100, 'C', 'K');
+    t.equal(typeof x, 'number', 'should return a number');
+
+    t.equal(x, 373.15, 'should return good number');
+
+    t.equal(temperature(773.15, 'K', 'C'), 500, 'should return good number');
+
+    t.end();
+
+  });
+
   t.end();
+
 });
