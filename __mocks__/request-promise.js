75aff9bbb3f5cb5f238d0d6e92d8de34237d7951
Rafał Pocztarski <r.pocztarski@gmail.com>
2018-07-31 02:32:46 +0200

    Remove mocked request tests from jest examples

--- __mocks__/request-promise.js
+++
@@ -1,8 +0,0 @@
-const data = require('../tests/jest/fixture1.json');
-
-const request = async () => {
-  return data;
-});
-
-module.exports = request;
-
--- tests/jest/converters.currency.test.js
+++ tests/jest/converters.currency.test.js
@@ -1,33 +1,16 @@
 'use strict';
 
-// const mock = require('mock-require');
-
-const data = require('./fixture1.json');
-
-beforeEach(() => {
-  jest.resetModules();
-});
-
-// mock('request-promise', async () => {
-//   return data;
-// });
-
-const { currency } = require('../../converters/currency');
-
 const example = x => new Promise(resolve => setTimeout(() => resolve(x + 1), 100));
 
 describe('example', () => {
-
   it('should work', async () => {
     expect(await example(10)).toBe(11);  
   });
-
 });
 
+// There are problems mocking requests with Jest
+// See examples in other testing frameworks
+
 describe('currency converter', async () => {
-  jest.doMock('request-promise', async () => {
-    return data;
-  });
-  const result = await currency(10, 'USD', 'PLN');
-  expect(result).toBe(1);
+
 });
